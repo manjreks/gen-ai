0a0738711c21e10c5021fb7b6dc1e6618c98498c
manjreks <santoshm.ds.21@gmail.com>
2025-03-27 14:11:32 -0400

    genai project

---
+++ simple_agent.py
@@ -0,0 +1,23 @@
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv()
+
+openai_key = os.getenv("OPENAI_API_KEY")
+
+llm_name="gpt-3.5-turbo"
+
+client = OpenAI(api_key=openai_key)
+
+response = client.chat.completions.create(
+    model=llm_name,
+    messages=[
+        {"role": "system", "content":"You are helpful assistant."},
+        {"role":"user", "content":"who is Nelson Mandela ?"}
+    ],
+)
+
+print(response.choices[0].message.content)
+
+
